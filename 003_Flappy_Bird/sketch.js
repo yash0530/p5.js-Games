119427fc4850886665579473ae6a42866437803e
Yash Jain <yashjain0530@gmail.com>
2018-03-16 01:48:01 +0530

    Blocks added

--- 003_Flappy_Bird/sketch.js
+++ 003_Flappy_Bird/sketch.js
@@ -1,19 +1,40 @@
+var count = 0;
+var num = 0;
+
 function setup() {
     createCanvas(window.innerHeight * 0.5625, window.innerHeight); // 16 : 9 vertical
     background(51);
-
+    frameRate(60);
     flappy = new Bird();
+    b = [];
 }
 
 function draw() {
     background(51);
     flappy.display();
     flappy.update();
+
+    if (count++ % 300 === 0) {
+        console.log("New block");
+        b[num++] = new Block();
+    }
+    for (var i = 0; i < b.length; i++) {
+        if (b[i] != null) {
+            b[i].display();
+            b[i].update();
+            if (b[i].isDead()) { b[i] = null; }
+        }
+    }
 }
 
 function keyPressed() {
     if (key === ' ') {
         flappy.tapped();
-        console.log("Space");
+        console.log("Space Pressed");
     }
+}
+
+function mousePressed() {
+    flappy.tapped();
+    console.log("Mouse Pressed");
 }
