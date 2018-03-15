9e5b34e19520b449eb0eefede86fbc4484788c2a
Yash Jain <yashjain0530@gmail.com>
2018-03-16 01:39:18 +0530

    Blocks Added

--- 003_Flappy_Bird/bird.js
+++ 003_Flappy_Bird/bird.js
@@ -1,14 +1,32 @@
+var count = 0;
+var num = 0;
+
 function setup() {
     createCanvas(window.innerHeight * 0.5625, window.innerHeight); // 16 : 9 vertical
     background(51);
-    frameRate(120);
+    frameRate(60);
     flappy = new Bird();
+    b = [];
 }
 
 function draw() {
     background(51);
     flappy.display();
     flappy.update();
+
+    if (count++ % 300 === 0) {
+        console.log("New block");
+        b[num++] = new Block();
+    }
+    for (var i = 0; i < b.length; i++) {
+        if (b[i] != null) {
+            b[i].display();
+            b[i].update();
+            if (b[i].isDead()) {
+                b[i] = null;
+            }
+        }
+    }
 }
 
 function keyPressed() {
