c25fb7e5f031c3e6814ac2d89bed67312ff6bef9
Yash Jain <yashjain0530@gmail.com>
2018-03-16 01:41:00 +0530

    Blocks Added
    this file contains the constructor function for the blocks

---
+++ 003_Flappy_Bird/block.js
@@ -0,0 +1,24 @@
+function Block() {
+    this.x = width;
+    this.y1 = 0;
+    this.h1 = Math.random() * height * 0.6;
+    this.y2 = this.h1 + height * 0.25;
+    this.h2 = height - this.y2;
+    this.width = width / 4;
+
+    this.display = function() {
+        fill(0, 255, 0);
+        rect(this.x, this.y1, this.width, this.h1);
+        rect(this.x, this.y2, this.width, this.h2);
+    }
+
+    this.update = function() {
+        this.x -= 1;
+    }
+
+    this.isDead = function() {
+        if (this.x < -this.width) {
+            return true;
+        } return false;
+    }
+}
